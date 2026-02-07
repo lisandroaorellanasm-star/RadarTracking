9e9836a45a4941751b0b0c3ea0d18878e009a091
Lisandro Orellana <lisandroaorellanasm@gmail.com>
2026-02-07 17:59:08 -0500

    Remove all Vite/React Router components - Next.js only

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ddb177f513b34528"; }
+/******/ 		__webpack_require__.h = function() { return "b88ed1e278a3a984"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
